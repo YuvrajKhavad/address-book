bd7965fa5870d865bc7bd23bf81fc152e36016ec
“yuvrajkhavad” <yuvraj.khavad@gmail.com>
2021-08-25 00:19:49 +0530

    project setup and fixed erros

---
+++ dashboard/tasks/update/fetch.php
@@ -0,0 +1,7 @@
+<?php
+	include('../../../include/db.php');
+	$email = $_POST['email'];
+	$res = mysqli_query($con, "SELECT * FROM persons WHERE Email='$email' ");
+	$array = mysqli_fetch_row($res);
+	echo json_encode($array);
+?>
