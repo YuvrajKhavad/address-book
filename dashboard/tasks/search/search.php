c6be995a664aa9d441fd0669a2206063e80293d4
“yuvrajkhavad” <yuvraj.khavad@gmail.com>
2021-11-02 01:08:51 +0530

    view change

--- dashboard/tasks/search/search.php
+++ dashboard/tasks/search/search.php
@@ -1,6 +1,7 @@
 <?php
 include('../../../include/db.php');
 $village_name = trim($_POST['search-village-name']);
+$village_id = trim($_POST['village-id']);
 $search_by_amount = trim($_POST['search-by-amout']);
 $search_by_name = trim($_POST['search-by-name']);
 
@@ -10,38 +11,38 @@ $search_by_name = trim($_POST['search-by-name']);
 $query = "SELECT * FROM persons, location WHERE location.ID = persons.Village_Name";
 
 //$query .= " WHERE";
-if(!empty($village_name) || !empty($search_by_amount) || !empty($search_by_name))
+/*if(!empty($village_name) || !empty($search_by_amount) || !empty($search_by_name))
 {
     $query .= " WHERE";
-}
+}*/
 
 if(!empty($village_name))
 {
-    $query .= "`Village_Name` = '".$village_name."'";
+    $query .= " AND persons.Village_Name = ".$village_id."";
 
-    if($search_by_amount || $search_by_name)
+    /*if($search_by_amount || $search_by_name)
     {
         $query .= " AND ";
-    }
+    }*/
 }
 
 if(!empty($search_by_amount))
 {
-    $query .= "`Amount` = '".$search_by_amount."'";
+    $query .= " AND  `Amount` = '".$search_by_amount."'";
 
-    if( $search_by_name)
+    /*if( $search_by_name)
     {
        $query .= " AND ";
-    }
+    }*/
 }
 
 if(!empty($search_by_name))
 {
-    $query .= " CONCAT(First_Name,' ',Last_Name,' ',Surname ) like '%" . $search_by_name . "%'";
+    $query .= " AND  CONCAT(First_Name,' ',Last_Name,' ',Surname ) like '%" . $search_by_name . "%'";
 }
 
 //$query .= " AND location.ID = persons.Village_Name";
-
+echo $query;
 $res = mysqli_query($con, $query);
 $count = 1;
 $tr = "";
